f7adf12aa1162f7725ad9efe9bdff243157138b2
Junho0111 <kt27473067@gmail.com>
2025-11-01 13:19:53 +0900

    기본구분자 덧셈기능 수정 및 예외 추가1

--- src/main/java/calculator/splitAndNumSum.java
+++ src/main/java/calculator/splitAndNumSum.java
@@ -12,24 +12,33 @@ public class splitAndNumSum {
     public int split(String string) {
         int stringTotalNum = 0;
 
+        // null 이거나 빈 문자열이 들어오면 0
+        if (string == null || string.isEmpty()) return 0;
+
         String[] numbers = string.split(",|:");
 
         for (String s : numbers) {
-            stringTotalNum = getStringTotalNum(stringTotalNum, s);
+            int number;
+
+            try {
+                if (s.isEmpty()) continue;
+
+                number = Integer.parseInt(s);
+
+                if (number < 0) throw new IllegalArgumentException("양수가 아닌 숫자가 포함되어 있습니다.");
+
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("구분자가 아닌 문자가 포함되어 있습니다.");
+            }
+
+            stringTotalNum += number;
         }
 
         return stringTotalNum;
     }
 
-    //분리된 string형 숫자를 int형으로 바꾸어 더하기 연산 후 반환
-    private static int getStringTotalNum(int stringTotalNum, String splitString) {
-        stringTotalNum += Integer.parseInt(splitString);
-        return stringTotalNum;
-    }
-
 }
 
-//잘못된 값을 입력한 경우 예외 처리.
 //"//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 지정할 수 있음
 
 
